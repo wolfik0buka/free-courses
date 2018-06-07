49dab75fed85ed15b3824072bf90e10d0de562eb
Egor <wolfik_buka@mail.ru>
2018-06-07 16:14:39 +0300

    Fixed comments and ;

--- task1.js
+++ task1.js
@@ -1,16 +1,16 @@
-var degree = parseInt(process.argv[3])
-var numbers = parseInt(process.argv[2])
+var degree = parseInt(process.argv[3]);
+var numbers = parseInt(process.argv[2]);
 var sum = 0;
 
-/*
+
 for (var counter = 1; counter <=numbers ; counter++)
 {
 	sum += Math.pow(counter, degree);
-}*/
-
+}
+/*
 for (; numbers > 0;numbers--)
 	{
 		sum += Math.pow(numbers, degree);
 	}
-	
+*/	
 process.stdout.write(String(sum) + '\n');
