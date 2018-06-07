dca2aa5f56154034031f6ce0cbf168021932a381
Egor <wolfik_buka@mail.ru>
2018-06-07 16:12:51 +0300

    Algorithm changed. Now: Begin from 1 and to input number/ Before: Begin from input number and to 1.

--- task1.js
+++ task1.js
@@ -2,8 +2,15 @@ var degree = parseInt(process.argv[3])
 var numbers = parseInt(process.argv[2])
 var sum = 0;
 
+/*
+for (var counter = 1; counter <=numbers ; counter++)
+{
+	sum += Math.pow(counter, degree);
+}*/
+
 for (; numbers > 0;numbers--)
 	{
 		sum += Math.pow(numbers, degree);
 	}
-process.stdout.write(String(sum));
+	
+process.stdout.write(String(sum) + '\n');
