aa1e9267f5d6fafb24f221a20ff68cfa42d09d92
Egor <wolfik_buka@mail.ru>
2018-06-08 16:02:29 +0300

    parseINT changed to parseFloat, cause

--- task1.js
+++ task1.js
@@ -1,16 +1,17 @@
-var degree = parseInt(process.argv[3]);
-var numbers = parseInt(process.argv[2]);
+var degree = parseFloat(process.argv[3]);
+var numbers = parseFloat(process.argv[2]);
 var sum = 0;
 
 
-for (var counter = 1; counter <=numbers ; counter++)
+for (var counter = 1; counter <= numbers ; counter++)
 {
 	sum += Math.pow(counter, degree);
 }
+
 /*
 for (; numbers > 0;numbers--)
 	{
 		sum += Math.pow(numbers, degree);
 	}
 */	
-process.stdout.write(String(sum) + '\n');
+console.log(sum);
