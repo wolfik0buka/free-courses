6a637686a90ba5a4be5d959956072f4d4a7cabb1
Egor <wolfik_buka@mail.ru>
2018-07-09 13:48:48 +0300

    Изменение структуры файлов

---
+++ javascript/level1/index.js
@@ -0,0 +1,15 @@
+let x=10;
+let s = "hello";
+let z;
+let n = null;
+let li = [1, 4, 5];
+let ob = {name:"Egor", age:18};
+var b = true;
+
+console.log( s +x);
+console.log( s +s);
+console.log( s +z);
+console.log( s +n);
+console.log( s +li);
+console.log( s +ob);
+console.log( s +b);
